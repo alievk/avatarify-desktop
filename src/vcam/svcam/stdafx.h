c6b92e0c7193120ee48251b7a6e1f31b9beebf09
Vladimir Ivashkin <illusionww@gmail.com>
2020-07-13 19:03:16 +0300

    svcam and winsdk 7.1

---
+++ src/vcam/svcam/stdafx.h
@@ -0,0 +1,27 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+
+// TODO: reference additional headers your program requires here
+#include <src/vcam/baseclasses/streams.h>
+#include <olectl.h>
+#include <initguid.h>
+#include <src/vcam/baseclasses/dllsetup.h>
+
+#include <dvdmedia.h>
+#include <math.h>
+#include <direct.h>
+#include <dos.h>
+
+#pragma warning(disable:4710)  // 'function': function not inlined (optimzation)
+
